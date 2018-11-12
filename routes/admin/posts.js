a123e9e5ea25e68e374d85eaeff7f43a8c7d30f3
John Nam <john@Johnui-MacBook-Pro.local>
2018-11-12 11:00:15 -0500

    Registration added

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -24,53 +24,82 @@ router.get('/', (req, res) => {
 
 router.get('/create', (req, res) => {
     Category.find({}).then(categories => {
-        res.render('admin/posts/create', {categories:categories});
+        res.render('admin/posts/create', { categories: categories });
     });
 });
 
 router.post('/create', (req, res) => {
-    
-    let filename = 'https://data.whicdn.com/images/310822942/superthumb.jpg?t=1523888483'
 
-    if (!isEmpty(req.files)) {
+    let errors = [];
 
-        let file = req.files.file;
-        filename = '/uploads/' + Date.now() + '-'+ file.name;
 
-        file.mv('./public' + filename, (err) => {
-            if (err) throw err;
-        }); 
+    if (!req.body.title) {
+
+        errors.push({ message: 'please add a title' });
+
     }
 
-    let allowComments = true;
-    if (req.body.allowComments) {
-        allowComments = true
+
+    if (!req.body.body) {
+
+        errors.push({ message: 'please add a description' });
+
+    }
+
+
+    if (errors.length > 0) {
+
+        res.render('admin/posts/create', {
+
+            errors: errors
+
+        });
+
     } else {
-        allowComments = false;
+        let filename = 'https://data.whicdn.com/images/310822942/superthumb.jpg?t=1523888483'
+
+        if (!isEmpty(req.files)) {
+
+            let file = req.files.file;
+            filename = '/uploads/' + Date.now() + '-' + file.name;
+
+            file.mv('./public' + filename, (err) => {
+                if (err) throw err;
+            });
+        }
+
+        let allowComments = true;
+        if (req.body.allowComments) {
+            allowComments = true
+        } else {
+            allowComments = false;
+        }
+
+        const newPost = new Post({
+            title: req.body.title,
+            status: req.body.status,
+            allowComments: allowComments,
+            body: req.body.body,
+            file: filename,
+            category: req.body.category,
+            date: Date.now()
+        });
+
+        newPost.save().then(savedPost => {
+            req.flash('success_message', `Post ${savedPost.title} was created successfully`);
+            res.redirect('/admin/posts');
+        }).catch(err => {
+            console.log(err, 'could not save post ');
+        });
     }
 
-    const newPost = new Post({
-        title: req.body.title,
-        status: req.body.status,
-        allowComments: allowComments,
-        body: req.body.body,
-        file:filename,
-        category: req.body.category, 
-        date: Date.now()
-    });
 
-    newPost.save().then(savedPost => {
-        req.flash('success_message', `Post ${savedPost.title} was created successfully`);
-        res.redirect('/admin/posts');
-    }).catch(err => {
-        console.log(err, 'could not save post ');
-    });
 });
 
 router.get('/edit/:id', (req, res) => {
     Post.findOne({ _id: req.params.id }).then(post => {
         Category.find({}).then(categories => {
-            res.render('admin/posts/edit', {post:post, categories:categories});
+            res.render('admin/posts/edit', { post: post, categories: categories });
         });
     });
 });
@@ -92,14 +121,14 @@ router.put('/edit/:id', (req, res) => {
 
             if (!isEmpty(req.files)) {
                 let file = req.files.file;
-                filename = '/uploads/' + Date.now() + '-'+ file.name;
+                filename = '/uploads/' + Date.now() + '-' + file.name;
                 post.file = filename;
                 file.mv('./public' + filename, (err) => {
                     if (err) throw err;
-                }); 
+                });
             }
             post.save().then(() => {
-                req.flash('success_message','Post was successfully updated');
+                req.flash('success_message', 'Post was successfully updated');
                 res.redirect('/admin/posts');
             });
         });
@@ -108,13 +137,13 @@ router.put('/edit/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     Post.findOne({ _id: req.params.id })
         .then(post => {
-            
-            fs.unlink(uploadDir + post.file, (err)=>{
-                req.flash('success_message','Post was successfully deleted');
+
+            fs.unlink(uploadDir + post.file, (err) => {
+                req.flash('success_message', 'Post was successfully deleted');
                 res.redirect('/admin/posts');
-            }); 
+            });
             post.delete();
         });
-        
+
 });
 module.exports = router;
--- routes/home/index.js
+++ routes/home/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+//Models
 const Post = require('../../models/Posts'); 
 const Category = require('../../models/Category');
+const User = require('../../models/User');
 
 //Routes
 router.all('/*', (req,res,next) => {
@@ -27,8 +30,62 @@ router.get('/register', (req,res) => {
     res.render('home/register');
 });
 
-router.get('/post', (req,res)=>{
-    res.render('home/post');
+router.post('/register', (req,res) => {
+    let errors = [];
+
+     
+    //ERROR MESSAGES
+    if (!req.body.firstName) {
+        errors.push({ message: 'please add a firstName' });
+    }
+    if (!req.body.lastName) {
+        errors.push({ message: 'please add a lastName' });
+    }
+    if (!req.body.email) {
+        errors.push({ message: 'please add a email' });
+    }
+    if (!req.body.password) {
+        errors.push({ message: 'please add a password' });
+    }
+    if (req.body.password !== req.body.passwordConfirm) {
+        errors.push({ message: 'Passwords Dont match' });
+    }
+
+    if (errors.length > 0) {
+        res.render('home/register', {
+            errors: errors,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+        });
+    }else{
+        User.findOne({email: req.body.email}).then(user => {
+            if(!user){
+                const newUser = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+        
+        
+                bcrypt.genSalt(10, (err, salt)=> {
+                    bcrypt.hash(newUser.password, salt, (err,hash)=> {
+                        newUser.password = hash;
+                        
+                        newUser.save().then(savedUser => {
+                            req.flash('success_message', 'You are now registered, please Login');
+                            res.redirect('/login');
+                        });
+                    });
+                });
+            }else {
+                req.flash('error_message', 'account exists');
+                res.redirect('/login');
+            }
+        });
+    }
 });
 
 router.get('/post/:id', (req,res)=>{
